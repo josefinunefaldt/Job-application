70e87dc0875fb624aff432f5b2430c2fc8a5b133
josefinunefaldt <josefin6@hotmail.com>
2025-01-16 16:59:23 +0100

    Create vite project  with routing

---
+++ Job-application-ui/vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+export default defineConfig({
+  plugins: [TanStackRouterVite(), react()],
+});
