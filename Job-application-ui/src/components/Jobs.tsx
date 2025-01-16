6a7bf13a45b9f0dba3d95fb0affc6b487de7c53c
josefinunefaldt <josefin6@hotmail.com>
2025-01-16 17:23:23 +0100

    Basic layout for index page

---
+++ Job-application-ui/src/components/Jobs.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const Jobs = () => (
+  <div className="overflow-x-auto">
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Job</th>
+          <th>Contact Person</th>
+          <th>Location</th>
+          <th>Notification</th>
+          <th>Deadline</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Test</th>
+          <td>Test</td>
+          <td>Test</td>
+          <td>Test</td>
+          <td>Test</td>
+        </tr>
+        <tr>
+          <th>Test</th>
+          <td>Test</td>
+          <td>Test</td>
+          <td>Test</td>
+          <td>Test</td>
+        </tr>
+        <tr>
+          <th>Test</th>
+          <td>Test</td>
+          <td>Test</td>
+          <td>Test</td>
+          <td>Test</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
--- Job-application-ui/src/routes/index.tsx
+++ Job-application-ui/src/routes/index.tsx
@@ -1,9 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Jobs } from "../components/Jobs";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hej</div>;
+  return (
+    <div className="mt-9 flex flex-col">
+      <div className="overflow-x-auto">
+        <button className="btn btn-neutral float-end mb-4 ml-auto">Add</button>
+        <Jobs />
+      </div>
+    </div>
+  );
 }
